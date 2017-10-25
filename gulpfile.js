76a4ed5150ef76d7cbe3d7875a6a12db2d11dfd5
Ole Ersoy <ole.ersoy@gmail.com>
2017-10-25 18:05:29 -0500

    Added deploy task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,5 @@ require('@superflycss/task-clean');
 require('@superflycss/task-build');
 require('@superflycss/task-test');
 require('@superflycss/task-lint');
+require('@superflycss/task-deploy');
+
